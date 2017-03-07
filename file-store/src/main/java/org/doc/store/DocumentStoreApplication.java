2e8b8fdfcd2ea65dc58d5245038e1e36fa6ec779
raja <reddyrk82@gmail.com>
2017-03-07 17:52:55 -0500

    adding multi part configurator

--- file-store/src/main/java/org/doc/store/DocumentStoreApplication.java
+++ file-store/src/main/java/org/doc/store/DocumentStoreApplication.java
@@ -1,29 +1,50 @@
 package org.doc.store;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-//@EnableAutoConfiguration
+// @EnableAutoConfiguration
 // @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
 // HibernateJpaAutoConfiguration.class })
 @SpringBootApplication
 @EnableWebMvc
 // @EnableJpaRepositories(basePackages = "org.movie.store.repository")
-public class DocumentStoreApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DocumentStoreApplication.class, args);
-	}
 
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurerAdapter() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**").allowedOrigins("http://localhost:8080");
-//				// registry.addMapping("/**").allowedOrigins("*"); // allows all
-//				// origins
-//			}
-//		};
-//	}
+public class DocumentStoreApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(DocumentStoreApplication.class, args);
+  }
+
+  // @Bean
+  // public WebMvcConfigurer corsConfigurer() {
+  // return new WebMvcConfigurerAdapter() {
+  // @Override
+  // public void addCorsMappings(CorsRegistry registry) {
+  // registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+  // // registry.addMapping("/**").allowedOrigins("*"); // allows all
+  // // origins
+  // }
+  // };
+  // }
+
+  @Bean
+  MultipartConfigElement multipartConfigElement() {
+    MultipartConfigFactory factory = new MultipartConfigFactory();
+    factory.setMaxFileSize("5120MB");
+    factory.setMaxRequestSize("5120MB");
+    return factory.createMultipartConfig();
+  }
+  
+  // @Bean
+  // public MultipartResolver multipartResolver() {
+  // org.springframework.web.multipart.commons.CommonsMultipartResolver multipartResolver = new
+  // org.springframework.web.multipart.commons.CommonsMultipartResolver();
+  // multipartResolver.setMaxUploadSize(1000000);
+  // return multipartResolver;
+  // }
 }
