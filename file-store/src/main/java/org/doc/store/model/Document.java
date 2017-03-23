f5c325a497dd4fcbb35a70626a69ce9e4bfdd32d
srikanth reddy elthem <srikanth reddy elthem>
2017-03-23 15:38:26 -0400

    added amazon s3 file store service and local file store service

--- file-store/src/main/java/org/doc/store/model/Document.java
+++ file-store/src/main/java/org/doc/store/model/Document.java
@@ -1,81 +1,128 @@
 package org.doc.store.model;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "document")
 @NamedQuery(name = "Document.findAll", query = "SELECT d FROM Document d")
 public class Document {
 
-  @Id
-  @Column(name = "id")
-  private String uuid;
+	@Id
+	@Column(name = "id")
+	private String uuid;
 
-  @Column(name = "doc_name")
-  private String docName;
+	@Column(name = "doc_name")
+	private String docName;
 
-  @Column(name = "doc_type")
-  private String docType;
+	@Column(name = "doc_type")
+	private String docType;
 
-  @Column(name = "doc_tag")
-  private String docTag;
+	@Column(name = "doc_tag")
+	private String docTag;
 
-  @Column(name = "doc_size")
-  private long docSize;
+	@Column(name = "doc_size")
+	private long docSize;
 
-  @Column(name = "doc_url")
-  private String docUrl;
+	@Column(name = "doc_url")
+	private String docUrl;
 
-  public String getUuid() {
-    return uuid;
-  }
+	@Column(name = "content_type")
+	private String contentType;
 
-  public void setUuid(String uuid) {
-    this.uuid = uuid;
-  }
+	@Transient
+	private String presignedUrl;
 
-  public String getDocName() {
-    return docName;
-  }
+	@Transient
+	private byte[] fileData;
 
-  public void setDocName(String docName) {
-    this.docName = docName;
-  }
+	public Document() {
+	}
 
-  public String getDocType() {
-    return docType;
-  }
+	public Document(byte[] fileData) {
+		this.fileData = fileData;
+	}
+	
+	public String getContentType() {
+		return contentType;
+	}
 
-  public void setDocType(String docType) {
-    this.docType = docType;
-  }
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
 
-  public String getDocTag() {
-    return docTag;
-  }
+	public String getPresignedUrl() {
+		return presignedUrl;
+	}
 
-  public void setDocTag(String docTag) {
-    this.docTag = docTag;
-  }
+	public void setPresignedUrl(String presignedUrl) {
+		this.presignedUrl = presignedUrl;
+	}
 
-  public long getDocSize() {
-    return docSize;
-  }
+	public byte[] getFileData() {
+		return fileData;
+	}
 
-  public void setDocSize(long docSize) {
-    this.docSize = docSize;
-  }
+	public void setFileData(byte[] fileData) {
+		this.fileData = fileData;
+	}
 
-  public String getDocUrl() {
-    return docUrl;
-  }
+	public String getUuid() {
+		return uuid;
+	}
 
-  public void setDocUrl(String docUrl) {
-    this.docUrl = docUrl;
-  }
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
 
+	public String getDocName() {
+		return docName;
+	}
+
+	public void setDocName(String docName) {
+		this.docName = docName;
+	}
+
+	public String getDocType() {
+		return docType;
+	}
+
+	public void setDocType(String docType) {
+		this.docType = docType;
+	}
+
+	public String getDocTag() {
+		return docTag;
+	}
+
+	public void setDocTag(String docTag) {
+		this.docTag = docTag;
+	}
+
+	public long getDocSize() {
+		return docSize;
+	}
+
+	public void setDocSize(long docSize) {
+		this.docSize = docSize;
+	}
+
+	public String getDocUrl() {
+		return docUrl;
+	}
+
+	public void setDocUrl(String docUrl) {
+		this.docUrl = docUrl;
+	}
+
+	public InputStream getInputStream() {
+		return new ByteArrayInputStream(fileData);
+	}
 }
--- file-store/src/main/java/org/doc/store/service/DocumentServiceImpl.java
+++ file-store/src/main/java/org/doc/store/service/DocumentServiceImpl.java
@@ -2,9 +2,7 @@ package org.doc.store.service;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -18,161 +16,147 @@ import org.doc.store.model.Document;
 import org.doc.store.repository.DocumentRepository;
 import org.doc.store.repository.SearchDocumentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Transactional
 public class DocumentServiceImpl implements DocumentService {
 
-  private static final String docStoreFolderPath =
-      System.getProperty("user.dir") + File.separator + "doc-store";
+	private static final String docStoreFolderPath = System.getProperty("user.dir") + File.separator + "doc-store";
 
-  @PostConstruct
-  public void init() {
-    File docStoreFolder = new File(docStoreFolderPath);
-    docStoreFolder.mkdirs();
-  }
+	@Autowired
+	protected DocumentRepository repository;
 
-  @Autowired
-  protected DocumentRepository repository;
+	@Autowired
+	protected SearchDocumentRepository searchRepository;
 
-  @Autowired
-  protected SearchDocumentRepository searchRepository;
+	@Autowired
+	@Qualifier(value = "LocalFileStoageService")
+	private FileStorageService fileStorageService;
 
-  @Override
-  public DocumentDto save(DocumentDto docDto) {
-    Document document = enrichDocument(docDto);
-    saveFile(docDto.getMultiPartfile(), document.getDocUrl());
-    Document savedDoc = repository.save(document);
-    DocumentDto savedDocDto = new DocumentDto();
-    try {
-      BeanUtilsBean.getInstance().copyProperties(savedDocDto, savedDoc);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    return savedDocDto;
-  }
+	@PostConstruct
+	public void init() {
+		File docStoreFolder = new File(docStoreFolderPath);
+		docStoreFolder.mkdirs();
+	}
 
-  private void saveFile(MultipartFile multiPartfile, String docUrl) {
-    FileOutputStream fos = null;
-    try {
-      fos = new FileOutputStream(new File(docUrl));
-      InputStream is = multiPartfile.getInputStream();
-      byte[] byteArr = new byte[10 * 1024];
-      int numOfBytesRead = is.read(byteArr);
-      while (numOfBytesRead != -1) {
-        fos.write(byteArr, 0, numOfBytesRead);
-        numOfBytesRead = is.read(byteArr);
-      }
-      fos.flush();
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      if (fos != null) {
-        try {
-          fos.close();
-        } catch (IOException e1) {
-          e1.printStackTrace();
-        }
-      }
-    }
-  }
+	@Override
+	public DocumentDto save(DocumentDto docDto) {
+		Document document = null;
+		DocumentDto savedDocDto = new DocumentDto();
+		
+			document = new Document();
+			document = enrichDocument(docDto);
+			fileStorageService.save(document);
+			Document savedDoc = repository.save(document);
+			try {
+			BeanUtilsBean.getInstance().copyProperties(savedDocDto, savedDoc);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return savedDocDto;
+	}
 
-  Document enrichDocument(DocumentDto docDto) {
-    Document document = new Document();
+	Document enrichDocument(DocumentDto docDto) {
+		Document document=null;
+		try{
+		document = new Document(docDto.getMultiPartfile().getBytes());
+		
+		if (StringUtils.isEmpty(docDto.getUuid())) {
+			String uuid = UUID.randomUUID().toString();
+			document.setUuid(uuid);
+		}
 
-    if (StringUtils.isEmpty(docDto.getUuid())) {
-      String uuid = UUID.randomUUID().toString();
-      document.setUuid(uuid);
-    }
+		if (docDto.getMultiPartfile() != null) {
+			document.setDocSize(docDto.getMultiPartfile().getSize());
+		}
 
-    if (docDto.getMultiPartfile() != null) {
-      document.setDocSize(docDto.getMultiPartfile().getSize());
-    }
+		String docName = docDto.getDocName();
+		if (StringUtils.isEmpty(docName)) {
+			document.setDocName(document.getUuid() + "-" + docDto.getMultiPartfile().getOriginalFilename());
+		} else {
+			document.setDocName(document.getUuid() + "-" + docName);
+		}
 
-    String docName = docDto.getDocName();
-    if (StringUtils.isEmpty(docName)) {
-      document
-          .setDocName(document.getUuid() + "-" + docDto.getMultiPartfile().getOriginalFilename());
-    } else {
-      document.setDocName(document.getUuid() + "-" + docName);
-    }
+		if (StringUtils.isEmpty(document.getDocUrl())) {
+			String docPath = FileUtil.buildFilePath(document.getDocName(), docStoreFolderPath);
+			document.setDocUrl(docPath);
+		}
 
-    if (StringUtils.isEmpty(document.getDocUrl())) {
-      String docPath = FileUtil.buildFilePath(document.getDocName(), docStoreFolderPath);
-      document.setDocUrl(docPath);
-    }
+		String originalFileName = docDto.getMultiPartfile().getOriginalFilename();
+		if (StringUtils.isEmpty(docDto.getDocType()) && document.getDocName().lastIndexOf(".") != -1) {
+			String docType = originalFileName.substring(originalFileName.lastIndexOf(".") + 1);
+			document.setDocType(docType);
+		} else {
+			document.setDocType(docDto.getDocType());
+		}
 
-    String originalFileName = docDto.getMultiPartfile().getOriginalFilename();
-    if (StringUtils.isEmpty(docDto.getDocType()) && document.getDocName().lastIndexOf(".") != -1) {
-      String docType = originalFileName.substring(originalFileName.lastIndexOf(".") + 1);
-      document.setDocType(docType);
-    } else {
-      document.setDocType(docDto.getDocType());
-    }
+		document.setDocTag(docDto.getDocTag());
+		
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return document;
+	}
 
-    document.setDocTag(docDto.getDocTag());
-    return document;
-  }
+	@Override
+	public DocumentDto getFile(String docUuid) {
+		Document document = repository.findOne(docUuid);
+		DocumentDto documentDto = new DocumentDto();
+		documentDto.setDocName(document.getDocName());
+		documentDto.setDocTag(document.getDocTag());
+		documentDto.setDocType(document.getDocType());
+		documentDto.setDocUrl(document.getDocUrl());
+		return documentDto;
+	}
 
-  @Override
-  public DocumentDto getFile(String docUuid) {
-    Document document = repository.findOne(docUuid);
-    DocumentDto documentDto = new DocumentDto();
-    documentDto.setDocName(document.getDocName());
-    documentDto.setDocTag(document.getDocTag());
-    documentDto.setDocType(document.getDocType());
-    documentDto.setDocUrl(document.getDocUrl());
-    return documentDto;
-  }
+	@Override
+	public List<DocumentDto> searchDocuments(DocumentDto documentDto) {
+		List<Document> documentList = searchRepository.searchByCriteria(documentDto);
+		if (!(null == documentList)) {
+			List<DocumentDto> documentDtoList = new ArrayList<DocumentDto>();
+			for (Document document : documentList) {
+				documentDto.setUuid(document.getUuid());
+				documentDto.setDocName(document.getDocName());
+				documentDto.setDocTag(document.getDocTag());
+				documentDto.setDocType(document.getDocType());
+				documentDto.setDocUrl(document.getDocUrl());
 
-  @Override
-  public List<DocumentDto> searchDocuments(DocumentDto documentDto) {
-    List<Document> documentList = searchRepository.searchByCriteria(documentDto);
-    if (!(null == documentList)) {
-      List<DocumentDto> documentDtoList = new ArrayList<DocumentDto>();
-      for (Document document : documentList) {
-        documentDto.setUuid(document.getUuid());
-        documentDto.setDocName(document.getDocName());
-        documentDto.setDocTag(document.getDocTag());
-        documentDto.setDocType(document.getDocType());
-        documentDto.setDocUrl(document.getDocUrl());
+				documentDtoList.add(documentDto);
+			}
+			return documentDtoList;
+		} else
+			return null;
+	}
 
-        documentDtoList.add(documentDto);
-      }
-      return documentDtoList;
-    } else
-      return null;
-  }
+	@Override
+	public DocumentDto getFileStream(String docUuid) {
+		DocumentDto documentDto = new DocumentDto();
+		Document document = repository.findOne(docUuid);
+		documentDto.setDocName(document.getDocName());
+		File fileToRead = new File(document.getDocUrl());
+		byte[] fileContentInBytes = new byte[(int) fileToRead.length()];
 
-  @Override
-  public DocumentDto getFileStream(String docUuid) {
-	  DocumentDto documentDto=new DocumentDto();
-    Document document = repository.findOne(docUuid);
-    documentDto.setDocName(document.getDocName());
-    File fileToRead = new File(document.getDocUrl());
-    byte[] fileContentInBytes = new byte[(int) fileToRead.length()];
-    
-    FileInputStream fis = null;
-    try {
-      fis = new FileInputStream(fileToRead);
-      fis.read(fileContentInBytes);
-     
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      if (fis != null) {
-        try {
-          fis.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      }
-    }
-    documentDto.setContent(fileContentInBytes);
-    return documentDto;
-  }
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(fileToRead);
+			fis.read(fileContentInBytes);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		documentDto.setContent(fileContentInBytes);
+		return documentDto;
+	}
 
 }
