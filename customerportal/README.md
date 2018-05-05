f67481cc943b42c6e027f233496206aeb4ede680
Raja <reddyrk82@gmail.com>
2018-05-05 13:14:57 -0400

    adding customer portal JS files.

--- customerportal/README.md
+++ customerportal/README.md
@@ -1,6 +1,7 @@
 # CenturyLinkCodingTest
-Used Spring Boot 2.0.1
-Used mySQL as the Database
+Used Spring Boot 1.5.12.RELEASE
+Support mySQL/H2 Database
 Built REST API for the given coding assignment
+Launchable class is CustomerPortalApplication
 
 
--- customerportal/src/main/resources/static/js/app/app.js
+++ customerportal/src/main/resources/static/js/app/app.js
@@ -2,11 +2,7 @@
 
 var demoApp = angular.module('demo', [ 'ui.bootstrap', 'demo.controllers', 'demo.services' ]);
 
-demoApp.constant("CONSTANTS", {
-	getUserByIdUrl : "/user/getUser/",
-	getAllUsers : "/user/getAllUsers",
-	saveUser : "/user/saveUser",
-	
+demoApp.constant("CONSTANTS", {	
 	getCustomerByIdUrl : "/customers/",
 	getAllCustomers : "/customers",
 	saveCustomer : "/customers",
